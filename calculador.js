9f70b05774a440ad4aa76f01db44a513cec96717
HenriqueLombardi <henrique_lombardi@estudante.sesisenai.org.br>
2026-04-01 11:11:25 -0300

    add - teminei

--- calculador.js
+++ calculador.js
@@ -1,4 +1,31 @@
-function soma (
-    let numero1 = parseFloat
-    let numero2 = parseFloat(document.getElementById"n1")
-)
+function soma(){
+    let numero1 = parseFloat(document.getElementById("n1").value);
+    let numero2 = parseFloat(document.getElementById("n2").value);
+    let result = numero1+numero2;
+    alert(result);
+     
+}
+
+function subtração(){
+    let numero1 = parseFloat(document.getElementById("n1").value);
+    let numero2 = parseFloat(document.getElementById("n2").value);
+    let result = numero1-numero2;
+    alert(result);
+}
+
+function multiplicação(){
+    let numero1 = parseFloat(document.getElementById("n1").value)
+    let numero2 = parseFloat(document.getElementById("n2").value)
+    let result = numero1*numero2;
+    alert(result);
+}
+
+function divisão(){
+    let numero1 = parseFloat(document.getElementById("n1").value)
+    let numero2 = parseFloat(document.getElementById("n2").value)
+    let result = numero1/numero2;
+    alert(result);
+}
+
+
+alert(result)
